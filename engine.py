40d98f5ee6d7a9b36744f3bba5356930c869fda8
Quang Anh <quanganhvp32401@gmail.com>
2023-07-26 22:18:19 +0700

    u

--- engine.py
+++ engine.py
@@ -751,8 +751,6 @@ def train_and_evaluate_new(model: torch.nn.Module, original_model: torch.nn.Modu
             for param in model_old.parameters():
                 param.requires_grad = False
 
-        print("mask_key", mask.keys())
-        print("omega_key", omega.keys())
 @torch.no_grad()
 def proxy_grad_descent(model: torch.nn.Module, model_old: torch.nn.Module, task_id, args, mask, omega):
 
