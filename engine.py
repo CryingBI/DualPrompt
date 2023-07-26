48eef8c8e31abd1da6ca0669a81a403cdcc98468
Quang Anh <quanganhvp32401@gmail.com>
2023-07-26 21:55:29 +0700

    u

--- engine.py
+++ engine.py
@@ -602,6 +602,8 @@ def train_and_evaluate_new(model: torch.nn.Module, original_model: torch.nn.Modu
                 print("freeze_keys",freeze.keys())
             else:
                 freeze = {}
+        else:
+            freeze = {}
         # Transfer previous learned prompt params to the new prompt
         if args.prompt_pool and args.shared_prompt_pool:
             if task_id > 0:
